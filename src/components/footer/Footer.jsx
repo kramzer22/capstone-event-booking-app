d829206f72ed4bd52e75bb61d3238a841b13746b
John Michael Basalo <jomaikoh+github@gmail.com>
2023-11-03 22:05:29 +0800

    updated index.css root colors

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,6 +12,20 @@ function Footer({ selectedPageViewState }) {
 
   return (
     <div className="footer">
+      <div className="footer-cta">
+        <div className="footer-cta-details">
+          <h2>Get in Touch with Our Sales Team</h2>
+          <button className="footer-cta-btn">Book Now</button>
+        </div>
+
+        <div className="footer-cta-img">
+          <img
+            src="https://scontent.fdvo5-1.fna.fbcdn.net/v/t1.6435-9/157021501_3792285737545318_3568598696731868493_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=be3454&_nc_eui2=AeGs22zSdJbLebJSDLgBUTd_nRktRD19M8GdGS1EPX0zwRUc3OxXFQ0LwGRMQazCVZskQvPqrMfHp2Gbc7ccA3Wz&_nc_ohc=Iddl_R9RcPoAX-PX6lP&_nc_ht=scontent.fdvo5-1.fna&oh=00_AfCfhof26lkdoIl6_D1X2tc0PJjEasdApwp61KV7GW9kQg&oe=656C3A2E"
+            alt=""
+          />
+        </div>
+      </div>
+
       <div className="footer-container">
         <div className="footer-main-container">
           <a onClick={() => selectPage("/contact")}>&gt; contact us</a>
