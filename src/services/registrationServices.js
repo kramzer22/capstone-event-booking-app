a09650195e983d29c2a6d829c05930389d8669f0
John Michael Basalo <jomaikoh+github@gmail.com>
2023-11-11 14:41:30 +0800

    updated clienregistration css and jsx

--- src/services/registrationServices.js
+++ src/services/registrationServices.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3001/api/client/";
-const tokenURL = "http://localhost:3001/api/token/register-client/";
+const baseURL = "https://capstone-backend-4pv2.onrender.com/api/client/";
+const tokenURL =
+  "https://capstone-backend-4pv2.onrender.com/api/token/register-client/";
 
 function createClient(registrationToken) {
   return axios.post(`${baseURL}register-client/?token_id=${registrationToken}`);
