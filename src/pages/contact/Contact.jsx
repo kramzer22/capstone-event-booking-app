56aa5ff79b19f300667b7a2d7231fcb6d19ae9bb
John Michael Basalo <jomaikoh+github@gmail.com>
2023-11-21 20:55:19 +0800

    added responsiveness to contact.css @media only screen and (max-width: 600px)

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -162,7 +162,7 @@ function Contact({ userCookieState }) {
         <div className="contact-form-ctr">
           <div className="contact-img-heads">
             <img
-              src="https://scontent.fdvo5-1.fna.fbcdn.net/v/t39.30808-6/396843789_742788224344348_526221888827160091_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE0mNnZcYpY6hL5_V5NXD5P_tUnHxuhVFn-1ScfG6FUWb0V_wZn99g36iIvGfW4f8B0il6Utx3puYAROWx3x4T3&_nc_ohc=5Yso1WhdusUAX_Om7bg&_nc_ht=scontent.fdvo5-1.fna&oh=00_AfCczT9PHKTkumRrHX6X0q4w9LcKwK0otp34Oh3aIMwp7Q&oe=6549E73F"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo3RNXnMyrqXdOCOSjsNQ7UNJ_E2sH2HX6Xg"
               alt=""
             />
             <img
@@ -170,7 +170,7 @@ function Contact({ userCookieState }) {
               alt=""
             />
             <img
-              src="https://scontent.fdvo5-1.fna.fbcdn.net/v/t39.30808-6/319357814_6588972931130659_6433058501188106892_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEvpIG0_81haXnObHSIA725VAHqSzgrwgdUAepLOCvCBypFa2mWtnYHQvf4--P7fp8k1B-Un1ilAcc1rztaijXV&_nc_ohc=MYJ6DGOsB4cAX8kh21m&_nc_ht=scontent.fdvo5-1.fna&oh=00_AfDzjzVhfO43Zau5qh3TKRz7RdJTJy9-R8xwlxPIx7-sww&oe=654A3780"
+              src="https://yt3.googleusercontent.com/ytc/APkrFKbVGvJNJxG8EWWdy2ODYVGdbt95dmcWIEwNYAnC=s900-c-k-c0x00ffffff-no-rj"
               alt=""
             />
           </div>
