41b62c41b02de3e1e7e59da2e031579a0cb14802
Mark David D. Panganiban <markdavidpanganiban1218@hotmail.com>
2023-11-22 15:58:38 +0800

    add date picker for booking procedure

--- src/pages/venue/components/VenueViewItem.jsx
+++ src/pages/venue/components/VenueViewItem.jsx
@@ -1,12 +1,17 @@
 import { useState, useRef } from "react";
 
+import BookPicker from "../../../components/book-picker/BookPicker";
+
+import objectHelperModule from "../../../helpers/objectHelperModule";
+
 import "./venueViewItem.css";
 
 function VenueViewItem({ setViewDisplay, venue }) {
+  const [bookDisplay, setBookDisplay] = useState(null);
   const [selectedImageIndex, setSelectedImageIndex] = useState(-1);
   const mainImageref = useRef(null);
 
-  const slectImageHandler = (index) => {
+  const selectImageHandler = (index) => {
     if (selectedImageIndex !== index) {
       setSelectedImageIndex(index);
       mainImageref.current.src = venue.images[index].link;
@@ -15,98 +20,110 @@ function VenueViewItem({ setViewDisplay, venue }) {
     }
   };
 
+  const bookPickerHandler = () => {
+    if (objectHelperModule.getCookie("userRole") === "client") {
+      setBookDisplay(<BookPicker setBookDisplay={setBookDisplay} />);
+    }
+  };
+
   return (
-    <div className="venue-view-container">
-      <div className="venue-view-body-container">
-        <div className="venue-view-body">
-          <div>
-            <h2>{venue.venue_name}</h2>
-            <p>{venue.complete_address}</p>
+    <>
+      <div className="venue-view-container">
+        <div className="venue-view-body-container">
+          <div className="venue-view-body">
+            <div>
+              <h2>{venue.venue_name}</h2>
+              <p>{venue.complete_address}</p>
+            </div>
+            <div className="venue-view-content-container">
+              {venue.images.length === 0 ? (
+                <div className="venue-view-image-container">
+                  <img className="venue-view-image" src="" alt="" />
+                </div>
+              ) : (
+                <div className="venue-view-image-container">
+                  <img
+                    className="venue-view-image"
+                    ref={mainImageref}
+                    src={venue.images.length > 0 ? venue.images[0].link : ""}
+                    alt=""
+                  />
+                  <ul className="venue-view-image-list">
+                    {venue.images.map((image, index) => (
+                      <li
+                        key={index}
+                        onClick={() => {
+                          selectImageHandler(index);
+                        }}
+                        style={{
+                          cursor: "pointer",
+                          border:
+                            index === selectedImageIndex
+                              ? "1px solid var(--accent)"
+                              : "none",
+                        }}
+                      >
+                        <img src={image.link} alt="" />
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              <div
+                className="venue-view-description"
+                dangerouslySetInnerHTML={{
+                  __html: `<div class="description-container">${venue.description.replace(
+                    /\n/g,
+                    "<br>"
+                  )}</div>`,
+                }}
+              />
+            </div>
+            <div className="venue-view-package">
+              <h3>Packages offer: {venue.packages.length}</h3>
+              <ul className="venue-view-package-container">
+                {venue.packages.map((item, index) => (
+                  <li className="venue-view-package-item" key={index}>
+                    <div className="venue-view-package-header">
+                      <h4>{item.name}</h4>
+                      <h5>
+                        price: ₱
+                        {item.price.toLocaleString("en-US", {
+                          minimumFractionDigits: 2,
+                        })}
+                      </h5>
+                      <button
+                        className="venue-view-package-book-request"
+                        onClick={() => bookPickerHandler()}
+                      >
+                        request book
+                      </button>
+                    </div>
+                    <div className="venue-view-package-body">
+                      <p>{item.description}</p>
+                      <ul>
+                        {item.inclusions.map((inclusion, i) => (
+                          <li key={i}>{inclusion}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <div className="venue-view-content-container">
-            {venue.images.length === 0 ? (
-              <div className="venue-view-image-container">
-                <img className="venue-view-image" src="" alt="" />
-              </div>
-            ) : (
-              <div className="venue-view-image-container">
-                <img
-                  className="venue-view-image"
-                  ref={mainImageref}
-                  src={venue.images.length > 0 ? venue.images[0].link : ""}
-                  alt=""
-                />
-                <ul className="venue-view-image-list">
-                  {venue.images.map((image, index) => (
-                    <li
-                      key={index}
-                      onClick={() => {
-                        slectImageHandler(index);
-                      }}
-                      style={{
-                        cursor: "pointer",
-                        border:
-                          index === selectedImageIndex
-                            ? "1px solid var(--accent)"
-                            : "none",
-                      }}
-                    >
-                      <img src={image.link} alt="" />
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <div
-              className="venue-view-description"
-              dangerouslySetInnerHTML={{
-                __html: `<div class="description-container">${venue.description.replace(
-                  /\n/g,
-                  "<br>"
-                )}</div>`,
-              }}
-            />
-          </div>
-          <div className="venue-view-package">
-            <h3>Packages offer: {venue.packages.length}</h3>
-            <ul className="venue-view-package-container">
-              {venue.packages.map((item, index) => (
-                <li className="venue-view-package-item" key={index}>
-                  <div className="venue-view-package-header">
-                    <h4>{item.name}</h4>
-                    <h5>
-                      price: ₱
-                      {item.price.toLocaleString("en-US", {
-                        minimumFractionDigits: 2,
-                      })}
-                    </h5>
-                    <button className="venue-view-package-book-request">
-                      request book
-                    </button>
-                  </div>
-                  <div className="venue-view-package-body">
-                    <p>{item.description}</p>
-                    <ul>
-                      {item.inclusions.map((inclusion, i) => (
-                        <li key={i}>{inclusion}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <button
+            className="venue-view-button-close"
+            onClick={() => {
+              setViewDisplay(null);
+            }}
+          >
+            X
+          </button>
         </div>
-        <button
-          className="venue-view-button-close"
-          onClick={() => {
-            setViewDisplay(null);
-          }}
-        >
-          X
-        </button>
       </div>
-    </div>
+      {bookDisplay}
+    </>
   );
 }
 
