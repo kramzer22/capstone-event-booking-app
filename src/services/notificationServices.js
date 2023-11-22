bea29f7d00ca96ddbf0fce1663cf7e3ef196da26
Mark David D. Panganiban <markdavidpanganiban1218@hotmail.com>
2023-11-23 00:06:31 +0800

    update

---
+++ src/services/notificationServices.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+import objectHelperModule from "../helpers/objectHelperModule";
+
+const notificationURL = "http://localhost:3001/api/notification/";
+
+const getUserToken = () => objectHelperModule.getCookie("userToken");
+
+function getNotification() {
+  return axios.get(
+    `${notificationURL}${objectHelperModule.getCookie(
+      "userRole"
+    )}/?token_id=${getUserToken()}`
+  );
+}
+
+export default { getNotification };
