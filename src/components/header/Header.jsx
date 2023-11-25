143f421a976ab5c5781f5781a9a966bf9307505c
John Michael Basalo <jomaikoh+github@gmail.com>
2023-11-25 09:22:53 +0800

    updated header.jsx and header.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -141,7 +141,7 @@ function Header({ userCookieState }) {
           <div ref={profileDropDownRef} className="profile-manager-container">
             <ul>
               <li>
-                <Link className="nav-link" to="host/venue-manager">
+                <Link className="nav-link" to="/host/venue-manager">
                   Venue Manager
                 </Link>
               </li>
@@ -272,11 +272,11 @@ function Header({ userCookieState }) {
         <div className="logo-container">
           <img src={headerLogoSVG} alt="" />
         </div>
-        <div className="navigation-links">
+        <div className="navigation-links mobile">
           <a className="nav-link" onClick={showMobileNavHandler}>
             Menu
           </a>
-          <ul ref={mobileNavRef} className="navigation-links-container">
+          <ul ref={mobileNavRef} className="navigation-links-container mobile">
             <li>
               <Link className="nav-link" to="/" onClick={scrollToTop}>
                 {objectHelperModule.getCookie("userRole") === "host"
